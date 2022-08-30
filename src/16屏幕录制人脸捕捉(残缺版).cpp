3dd4c10f543884893f715fdca50027f9503c5c8b
归故里 <97590219+13870517674@users.noreply.github.com>
2022-08-30 18:49:39 +0800

    无意义的修改

--- src/16屏幕录制人脸捕捉(残缺版).cpp
+++ src/16屏幕录制人脸捕捉(残缺版).cpp
@@ -1,8 +1,9 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include<filesystem>
+#include<thread>
 
-void faceRecognition(cv::Mat& srcImage) {//单独封装一个精简的函数(使用的15)
+bool faceRecognition(cv::Mat& srcImage) {//单独封装一个精简的函数(使用的15)
     // 加载分类器
     cv::CascadeClassifier cascade;
     auto i = cascade.load("haarcascade_frontalface_alt.xml");
@@ -25,9 +26,11 @@ void faceRecognition(cv::Mat& srcImage) {//单独封装一个精简的函数(使
         rectangle(dstImage, cv::Rect(center.x - rect[i].width * 0.5, center.y - rect[i].height * 0.5, rect[i].width, rect[i].height), cv::Scalar(30, 144, 255), 2);//(方框)
     }
     srcImage = dstImage;
+    return rect.size() != 0;
 }
 
 int main() {
+    std::vector<cv::Mat>mat;
     //视频保存位置(不支持avi格式)
     std::string outputVideoPath = "video/人脸识别.mp4";
     //打开摄像头
@@ -41,14 +44,13 @@ int main() {
     outputVideo.open(outputVideoPath, -1, 24, S, true);
 
     cv::Mat frameImage;
-
+    int  n = 1;
     while (true) {
         //读取当前帧
         capture0 >> frameImage;
-        if (frameImage.empty()) break;
-
+       /* if (frameImage.empty()) break;*/
         //输出当前帧
-        //faceRecognition(frameImage);
+        faceRecognition(frameImage);
         cv::imshow("output", frameImage);
         //保存当前帧，不停的写入到(test.mp4)文件
         outputVideo << frameImage;
